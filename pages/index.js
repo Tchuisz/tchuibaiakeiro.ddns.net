e9da60667f9e9037e6c0eb83f89f87abbf6f0740
Tchuisz <140774634+Tchuisz@users.noreply.github.com>
2023-09-02 12:43:20 -0300

    God Tchui

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,6 @@ export default function Home() {
       <main>
         <Header title="Bem Vindo Baiakeiro!" />
         <p className="description">
-          o servidor ainda não tem site mais vou ajudar vocês a entrar no server pelo tíbia 8.6 + ipchange ou apk Otcliente e bem simples fazer as conf abre seu tibia 8.6 Coloque na account 1 password 1 para criar sua senha. No Apk Android você deve por esse ip tchuibaiakeiro.ddns.net:7171 e seu apk estiver já com outros opção você deve achar na lista de server um nome chamado Another aki você coloca o ip tchuibaiakeiro.ddns.net:7171 na versão coloca 860 pra criar sua account Basta por na account 1 password 1 Criando sua account no game. depois entre no game com sua account e password que voce acabou de criar dentro do game. Pronto agora só logar e vir jogar com a galera por que Baiakeiro e por Baiakeiro!!
-        </p>
       </main>
 
       <Footer />
